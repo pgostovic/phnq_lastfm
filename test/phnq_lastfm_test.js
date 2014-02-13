87d818705052333932f78a6276dabd080c976558
Patrick Gostovic <pg@phranq.com>
2014-02-12 19:37:54 -0500

    Added some stuff.

---
+++ test/phnq_lastfm_test.js
@@ -0,0 +1,14 @@
+var assert = require("assert");
+var phnq_lastfm = require("../phnq_lastfm");
+
+describe("phnq_lastfm", function()
+{
+	describe("bubba", function()
+	{
+		it("should extend the destination object with the source object's keys and values", function()
+		{
+			assert.equal(1, 1);
+		});
+	});
+});
+
