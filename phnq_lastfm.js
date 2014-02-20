e25aa70e9aa51ca687e7f0be4a904fcf6cba6aa9
Patrick Gostovic <pg@phranq.com>
2014-02-20 18:31:16 -0500

    Added some basic functionality and unit tests.

--- phnq_lastfm.js
+++ phnq_lastfm.js
@@ -1,1 +1,91 @@
-module.exports = {};
+var phnq_core = require("phnq_core");
+var request = require("request");
+var _ = require("underscore");
+
+var BASE_URL = "http://ws.audioscrobbler.com/2.0/";
+
+module.exports = phnq_core.clazz(
+{
+	init: function(apiKey, username)
+	{
+		if(!apiKey)
+		{
+			throw "You must supply a last.fm api key";
+		}
+		this.apiKey = apiKey;
+		this.username = username;
+	},
+	
+	req: function(method, params, fn)
+	{
+		params = params || {};
+		params["api_key"] = this.apiKey;
+		params["method"] = method;
+		params["format"] = "json";
+		request.get(BASE_URL, {qs:params, json:true}, function(err, res, body)
+		{
+			fn(err, body);
+		});
+	},
+	
+	artistSearch: function(q, fn)
+	{
+		var _this = this;
+		this.req("artist.search", {"artist":q, "limit": 10}, function(err, body)
+		{
+			if(err)
+			{
+				return fn(err);
+			}
+			var artists = [];
+			_.each(body.results.artistmatches.artist, function(artistData)
+			{
+				artists.push(new Artist(artistData, _this));
+			});
+			fn(null, artists);
+		})
+	},
+	
+	artistFind: function(arg, fn)
+	{
+		var _this = this;
+		var params = arg.mbid ? {mbid:arg.mbid} : {artist:arg, autocorrect:1}
+		if(this.username)
+		{
+			params.username = this.username;
+		}
+		this.req("artist.getinfo", params, function(err, respObj)
+		{
+			if(err)
+			{
+				return fn(err);
+			}
+			fn(null, new Artist(respObj.artist, _this));
+		});
+	}
+});
+
+var Artist = phnq_core.clazz(
+{
+	init: function(artistData, client)
+	{
+		this.client = client;
+		phnq_core.extend(this, artistData);
+	},
+	
+	loadArtistInfo: function(fn)
+	{
+		var _this = this;
+		var arg = this.mbid ? {mbid:this.mbid} : this.name;
+		
+		this.client.artistFind(arg, function(err, artistInfo)
+		{
+			if(err)
+			{
+				return fn(err, null);
+			}
+			phnq_core.extend(_this, artistInfo);
+			fn(null, _this);
+		})
+	}
+});
--- test/phnq_lastfm_test.js
+++ test/phnq_lastfm_test.js
@@ -1,13 +1,97 @@
 var assert = require("assert");
-var phnq_lastfm = require("../phnq_lastfm");
+var LastfmClient = require("../phnq_lastfm");
+
+var LAST_FM_API_KEY = process.env["LAST_FM_API_KEY"];
+var MBID_PAINS_OF_BEING_PURE_AT_HEART = "46f4e349-ddec-4131-9367-91b630a821d0";
 
 describe("phnq_lastfm", function()
 {
-	describe("bubba", function()
+	describe("instantiation", function()
 	{
-		it("should extend the destination object with the source object's keys and values", function()
+		it("should throw an error when instantiated without an api key", function()
 		{
-			assert.equal(1, 1);
+			assert.throws(function()
+			{
+				new LastfmClient();
+			});
+		});
+
+		it("should not throw an error when instantiated with an api key", function()
+		{
+			assert.doesNotThrow(function()
+			{
+				new LastfmClient(LAST_FM_API_KEY);
+			});
+		});
+	});
+	
+	describe("artist search", function()
+	{
+		it("should find the artist The Pains of Being Pure At Heart by searching, and then fully load the artist info.", function(done)
+		{
+			var client = new LastfmClient(LAST_FM_API_KEY);
+			client.artistSearch("The Pains of Being Pure At Heart", function(err, artists)
+			{
+				assert.ifError(err);
+				assert(artists.length > 0);
+
+				var artist = artists[0];
+				assert.equal("The Pains of Being Pure At Heart", artist.name)
+				assert.ok(!artist.bio);
+				
+				artist.loadArtistInfo(function(err, theArtist)
+				{
+					assert.ifError(err);
+					assert.equal(artist, theArtist);
+					
+					assert.ok(!!artist.bio);
+					
+					done();
+				});
+			});
+		});
+	});
+	
+	describe("artist find", function()
+	{
+		it("should find an artist by name", function(done)
+		{
+			var client = new LastfmClient(LAST_FM_API_KEY);
+			client.artistFind("The Pains of Being Pure At Heart", function(err, artist)
+			{
+				assert.ifError(err);
+				assert.equal("The Pains of Being Pure At Heart", artist.name);
+				assert.equal(MBID_PAINS_OF_BEING_PURE_AT_HEART, artist.mbid);
+				assert.ok(!!artist.bio);
+				done();
+			});
+		});
+
+		it("should find an artist by slightly incorrect name", function(done)
+		{
+			var client = new LastfmClient(LAST_FM_API_KEY);
+			// Missing 's' on Pain(s)
+			client.artistFind("The Pain of Being Pure At Heart", function(err, artist)
+			{
+				assert.ifError(err);
+				assert.equal("The Pains of Being Pure At Heart", artist.name)
+				assert.equal(MBID_PAINS_OF_BEING_PURE_AT_HEART, artist.mbid);
+				assert.ok(!!artist.bio);
+				done();
+			});
+		});
+		
+		it("should find an artist by mbid", function(done)
+		{
+			var client = new LastfmClient(LAST_FM_API_KEY);
+			client.artistFind({mbid:MBID_PAINS_OF_BEING_PURE_AT_HEART}, function(err, artist)
+			{
+				assert.ifError(err);
+				assert.equal("The Pains of Being Pure At Heart", artist.name);
+				assert.equal(MBID_PAINS_OF_BEING_PURE_AT_HEART, artist.mbid);
+				assert.ok(!!artist.bio);
+				done();
+			});
 		});
 	});
 });
